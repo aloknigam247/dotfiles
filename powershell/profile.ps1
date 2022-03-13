cfbff5bc3d6404d4801083b5da73beb3796b4bb2
Alok Nigam <aloknigam247@gmail.com>
2022-03-13 15:03:13 +0530

    13-03-2022 updates

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -22,6 +22,7 @@ Set-Location -
 
 # Aliases
 Set-Alias -Name v -Value nvim
+Set-Alias -Name choco -Value 'C:\ProgramData\chocolatey\bin\choco.exe'
 
 Import-Module oh-my-posh
 oh-my-posh --init --shell pwsh --config C:\Users\aloknigam\.oh-my-posh\themes\rudolfs-light.omp.json | Invoke-Expression
