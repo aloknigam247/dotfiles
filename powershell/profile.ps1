192df64ac9c6e9fa5e5ab1480ca03d7725ce42ab
Alok Nigam <aloknigam247@gmail.com>
2022-03-24 19:27:24 +0530

    24-03-2022 updates

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -17,10 +17,20 @@ Start-Job {
         git add .
         git commit -m "$dt updates"
         git push
-        $balmsg.ShowBalloonTip(0, "dotfiles synced", "", [System.Windows.Forms.ToolTipIcon]::Info)
+
+        # Send ballon notification
+        Add-Type -AssemblyName System.Windows.Forms
+        $global:balmsg = New-Object System.Windows.Forms.NotifyIcon
+        $path = (Get-Process -id $pid).Path
+        $balmsg.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
+        $balmsg.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Info
+        $balmsg.BalloonTipText = " "
+        $balmsg.BalloonTipTitle = "dotfiles updated"
+        $balmsg.Visible = $true
+        $balmsg.ShowBalloonTip(20000)
     }
+
 }
-        $balmsg.ShowBalloonTip(0, "dotfiles synced", "", [System.Windows.Forms.ToolTipIcon]::Info)
 
 # Aliases
 Set-Alias -Name v -Value nvim
