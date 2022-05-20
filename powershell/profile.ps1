ef788523f7e5792ca479fc6c1878787a0b3d2764
Alok Nigam <aloknigam@microsoft.com>
2022-05-20 11:49:22 +0530

    20-05-2022 updates

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -109,4 +109,4 @@ function prompt {
 }
 
 $readline = Get-PSReadLineOption
-Set-PSReadLineOption -Colors @{ Command = $PSStyle.Italic + $readline.CommandColor + $PSStyle.ItalicOff }
+Set-PSReadLineOption -Colors @{ Command = $PSStyle.Italic + $readline.CommandColor }
