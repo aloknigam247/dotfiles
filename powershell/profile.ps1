6064f37b1f3a39dc8132a24e464ca0bae0a61da7
Alok Nigam <aloknigam@microsoft.com>
2022-07-11 11:16:42 +0530

    Updated at 07/11/2022 11:16:41

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -119,6 +119,17 @@ Set-PSReadLineOption -PredictionSource HistoryAndPlugin -PredictionViewStyle Lis
 # Posh-git
 Import-Module C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1
 
+# winget tab completion
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+        $Local:word = $wordToComplete.Replace('"', '""')
+        $Local:ast = $commandAst.ToString().Replace('"', '""')
+        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
+
 
 # Prompt Styling
 # ``````````````
