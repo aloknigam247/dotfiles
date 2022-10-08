eaa9a539889cc21af3f72bfdcbf9720cdc6b0bda
Alok Nigam <aloknigam247@gmail.com>
2022-10-08 19:07:20 +0530

    Updated at 10/08/2022 19:07:15

--- neovim/plugins.lua
+++ neovim/plugins.lua
@@ -690,7 +690,40 @@ use {
 --             require('nvim-custom-diagnostic-highlight').setup {}
 --         end
 --     }
-use 'neovim/nvim-lspconfig'
+use {
+    'neovim/nvim-lspconfig',
+    config = function()
+        -- TODO: right click options
+        -- vim.cmd [[autocmd CursorHold * lua vim.diagnostic.open_float(nil, {focus=false})]]
+        local opts = { noremap=true, silent=true }
+        -- vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+        vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+        -- vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+        vim.diagnostic.config({
+            float = {
+                source = true
+            },
+            severity_sort = true,
+            virtual_text = {
+                prefix = '',
+                source = true
+            }
+        })
+
+        vim.cmd [[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]]
+        vim.cmd [[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
+
+        vim.cmd[[
+        sign define DiagnosticSignError text= texthl=DiagnosticSignError linehl= numhl=
+        sign define DiagnosticSignWarn text= texthl=DiagnosticSignWarn linehl= numhl=
+        sign define DiagnosticSignInfo text= texthl=DiagnosticSignInfo linehl= numhl=
+        sign define DiagnosticSignHint text= texthl=DiagnosticSignHint linehl= numhl=
+        ]]
+
+    end
+}
 use {
     'williamboman/mason.nvim',
     config = function()
@@ -700,12 +733,55 @@ use {
 use {
     'williamboman/mason-lspconfig.nvim',
     config = function()
+        -- Use an on_attach function to only map the following keys
+        -- after the language server attaches to the current buffer
+        local on_attach = function(client, bufnr)
+            -- Enable completion triggered by <c-x><c-o>
+            vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+            -- Mappings.
+            -- See `:help vim.lsp.*` for documentation on any of the below functions
+            local bufopts = { noremap=true, silent=true, buffer=bufnr }
+            -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+            -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+            -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+            -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+            -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+            -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+            -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+            -- vim.keymap.set('n', '<space>wl', function()
+            --     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+            -- end, bufopts)
+            -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+            -- vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+            -- vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+            -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+            -- vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+        end
+        local border = {
+            { '╭', 'FloatBorder' },
+            { '─', 'FloatBorder' },
+            { '╮', 'FloatBorder' },
+            { '│', 'FloatBorder' },
+            { '╯', 'FloatBorder' },
+            { '─', 'FloatBorder' },
+            { '╰', 'FloatBorder' },
+            { '│', 'FloatBorder' },
+        }
+        -- LSP settings (for overriding per client)
+        local handlers =  {
+            ["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, {border = border}),
+            ["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {border = border }),
+        }
+        -- Add additional capabilities supported by nvim-cmp
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
         require("mason-lspconfig").setup()
         require("mason-lspconfig").setup_handlers {
             function (server_name)
                 require("lspconfig")[server_name].setup {
-                    on_attach = on_attach,
-                    capabilities = capabilities
+                    capabilities = capabilities,
+                    handlers = handlers,
+                    on_attach = on_attach
                 }
             end
         }
@@ -2341,13 +2417,6 @@ end
 --         bundle_path = vim.fn.stdpath("data") .. "\\mason\\packages\\powershell-editor-services\\",
 --         cmd = {'pwsh', '-NoLogo', '-NoProfile', '-Command', "C:\\Users\\aloknigam\\AppData\\Local\\nvim-data\\mason\\packages\\powershell-editor-services\\PowerShellEditorServices\\Start-EditorServices.ps1"}
 --     }) ]]
-
-vim.diagnostic.config({
-    virtual_text = {
-        source = true
-    },
-    severity_sort = true
-})
 
 
 -- vim: fmr=</>,<~>  fdm=marker
