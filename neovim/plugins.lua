bad8a126006c814ce2b5e31c7498c388e68e7f42
Alok Nigam <aloknigam247@gmail.com>
2022-11-16 10:59:13 +0530

    Updated at 11/16/2022 10:59:13

--- neovim/plugins.lua
+++ neovim/plugins.lua
@@ -1472,7 +1472,7 @@ use {
                         'filename',
                         file_status = true,      -- Displays file status (readonly status, modified status)
                         newfile_status = false,   -- Display new file status (new file means no write after created)
-                        path = 3,                -- 0: Just the filename
+                        path = 0,                -- 0: Just the filename
                         -- 1: Relative path
                         -- 2: Absolute path
                         -- 3: Absolute path, with tilde as the home directory
