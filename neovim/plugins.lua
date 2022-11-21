7af21828e6a09740d39f259dc5483a9131f09acd
Alok Nigam <aloknigam247@gmail.com>
2022-11-21 10:34:12 +0530

    Updated at 11/21/2022 10:34:12

--- neovim/plugins.lua
+++ neovim/plugins.lua
@@ -827,6 +827,25 @@ use {
             },
             current_line_blame_formatter = '  <author>  <committer_time>  <summary>`'
         }
+
+        -- Navigation
+        local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+        end
+
+        map('n', ']c', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+        end, {expr=true})
+
+        map('n', '[c', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+        end, {expr=true})
     end
 }
 
