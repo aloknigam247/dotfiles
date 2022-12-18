3111721f1e5f3f739909eb413b45fb2da03b134f
Alok Nigam <aloknigam247@gmail.com>
2022-12-18 13:46:43 +0530

    Updated at 12/18/2022 13:46:42

--- neovim/plugins.lua
+++ neovim/plugins.lua
@@ -984,6 +984,10 @@ use {
                         capabilities = capabilities,
                         handlers = handlers,
                         on_attach = on_attach,
+                        on_init = function(client, result)
+                            print(vim.inspect(client))
+                            print(vim.inspect(result))
+                        end,
                         enable_ms_build_load_projects_on_demand = true,
                         organize_imports_on_format = true
                     }
