715420ce8eea6dd53fe61b3757eb96ef3eed7778
Alok Nigam <aloknigam247@gmail.com>
2022-12-22 19:52:15 +0530

    Updated at 12/22/2022 19:52:15

--- neovim/lazyplugins.lua
+++ neovim/lazyplugins.lua
@@ -1022,6 +1022,7 @@ Plugins = {
                         capabilities = capabilities,
                         handlers = handlers,
                         on_attach = on_attach,
+                        root_dir = require('lspconfig.util').find_git_ancestor,
                         enable_ms_build_load_projects_on_demand = true,
                         organize_imports_on_format = true
                     }
