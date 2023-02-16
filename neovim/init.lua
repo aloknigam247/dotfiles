65d6f54efb480e37548a36fe8f9b20246ffde2eb
Alok Nigam <aloknigam@microsoft.com>
2023-02-16 12:07:33 +0530

    Updated at 02/16/2023 12:07:32

--- neovim/init.lua
+++ neovim/init.lua
@@ -526,6 +526,43 @@ function FixVisual()
     vim.api.nvim_set_hl(0, "Visual", { bg = bg })
 end
 
+function SeniorMarsTheme()
+    require("gruvbox").setup({
+        overrides = {
+            Normal = {bg = "#0E1018"},
+            VertSplit = {bg = '#0E1018'},
+            SignColumn = {bg = "#ff9900"},
+            Define = {link = "GruvboxPurple"},
+            Macro = {link = "GruvboxPurple"},
+            ["@constant.builtin"] = { link = "GruvboxPurple"},
+            ["@storageclass.lifetime"] = { link = "GruvboxAqua"},
+            ["@text.note"] = { link = "TODO" },
+            CocCodeLens = {fg = "#878787"},
+            ContextVt = {fg = "#878787"},
+            Comment = {fg = "#fe8019", italic = true},
+            Folded = {italic = true, fg = "#fe8019", bg = "#3c3836"},
+            FoldColumn = {fg = "#fe8019", bg = "#0E1018"},
+            DiffAdd = {bold = true, reverse = false, fg = "", bg = "#2a4333"},
+            DiffChange = {bold = true, reverse = false, fg = "", bg = "#333841"},
+            DiffDelete = { bold = true, reverse = false, fg = "#442d30", bg = "#442d30"},
+            DiffText = {bold = true, reverse = false, fg = "", bg = "#213352"},
+            StatusLine = {bg = "#ffffff", fg = "#0E1018"},
+            StatusLineNC = {bg = "#3c3836", fg = "#0E1018"},
+            CursorLineNr = {fg = "#fabd2f", bg = "#0E1018"},
+            CocWarningFloat = {fg = "#dfaf87"},
+            CocInlayHint = {fg = "#87afaf"},
+            DiagnosticVirtualTextWarn = {fg = "#dfaf87"},
+            GruvboxOrangeSign = {fg = "#dfaf87", bg = "#0E1018"},
+            GruvboxAquaSign = {fg = "#8EC07C", bg = "#0E1018"},
+            GruvboxGreenSign = {fg = "#b8bb26", bg = "#0E1018"},
+            GruvboxRedSign = {fg = "#fb4934", bg = "#0E1018"},
+            GruvboxBlueSign = {fg = "#83a598", bg = "#0E1018"},
+            WilderMenu = {fg = "#ebdbb2", bg = "#0E1018"},
+            WilderAccent = {fg = "#f4468f", bg = "#0E1018"}
+        }
+    })
+end
+
 -- https://github.com/lifepillar/vim-colortemplate
 local colos = {}
 
@@ -687,6 +724,7 @@ Dark  { 'forestbones',                'zenbones'     }
 Light { 'forestbones',                'zenbones'     }
 Dark  { 'github_dark',                'github'       }
 Light { 'github_light',               'github'       }
+Dark  { 'gruvbox',                    '_',           pre  = SeniorMarsTheme    }
 Dark  { 'gruvbox',                    '_'            }
 Light { 'gruvbox',                    '_'            }
 Dark  { 'gruvbox-baby',               '_'            }
@@ -2240,7 +2278,7 @@ AddPlugin {
 }
 -- <~>
 --━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━     Rooter     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</>
--- TOOD: should not be default
+-- TODO: should not be default
 vim.api.nvim_create_autocmd('BufRead', { pattern = '*', callback = function()
     local filepath = vim.fn.bufname('%')
     if filepath:sub(1, 1) ~= '/' and filepath:sub(2, 2) ~= ':' then
@@ -2963,7 +3001,8 @@ vim.notify = function(msg, level, opt)
 end
 -- <~>
 --━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━    Utilities   ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</>
--- use 'AckslD/nvim-trevJ.lua'
+-- TODO: use 'AckslD/nvim-trevJ.lua'
+-- TODO: https://github.com/wellle/targets.vim
 
 AddPlugin {
     'AndrewRadev/inline_edit.vim',
