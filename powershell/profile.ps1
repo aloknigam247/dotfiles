2aaa92de5bb5c96eb3f41de5b432cb22a9d558a3
Alok Nigam <aloknigam@microsoft.com>
2023-03-18 00:47:06 +0530

    Updated at 03/18/2023 00:47:06

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -166,6 +166,10 @@ function desktop {
 }
 
 # Git functions
+function gl {
+    git log --color=always --pretty="%C(#B4AD2D) %C(#DBD56E)%h %Creset- %C(#88AB75) %s %C(#216E82)󰔟 %ar on %ah %C(#7D7C84)<%C(#ACABB0)%an %C(#7D7C84) %ae>%C(#DE8F6E)%d" $args
+}
+
 function gs {
     git status --ignore-submodules=all --short --branch --show-stash --ahead-behind $args
 }
