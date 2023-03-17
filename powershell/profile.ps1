22c49b02ac1a0b59a7b10421883a50074029b538
Alok Nigam <aloknigam@microsoft.com>
2023-03-18 00:48:46 +0530

    Updated at 03/18/2023 00:48:46

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -166,8 +166,7 @@ function desktop {
 }
 
 # Git functions
-Remove-Alias gl
-function gl {
+function glog {
     git log --color=always --pretty="%C(#B4AD2D) %C(#DBD56E)%h %Creset- %C(#88AB75) %s %C(#216E82)󰔟 %ar on %ah %C(#7D7C84)<%C(#ACABB0)%an %C(#7D7C84) %ae>%C(#DE8F6E)%d" $args
 }
 
