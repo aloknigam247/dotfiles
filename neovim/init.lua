e0dc9e37f692406598497b179f95b737d0eecb7b
Alok Nigam <aloknigm247@gmail.com>
2023-10-06 21:43:15 +0530

    Updated at 10/06/2023 21:43:15

--- neovim/init.lua
+++ neovim/init.lua
@@ -2525,54 +2525,54 @@ AddPlugin {
 }
 
 -- configure or remove
-AddPlugin {
-    'kevinhwang91/nvim-ufo',
-    -- cond = function()
-    --     return vim.o.foldmethod ~= 'marker'
-    -- end,
-    config = function()
-        vim.o.foldcolumn = '1' -- '0' is not bad
-        vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-        vim.o.foldlevelstart = 99
-        vim.o.foldenable = true
-        require('ufo').setup({
-            fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
-                local newVirtText = {}
-                local suffix = (' 󰁂 %d '):format(endLnum - lnum)
-                local sufWidth = vim.fn.strdisplaywidth(suffix)
-                local targetWidth = width - sufWidth
-                local curWidth = 0
-                for _, chunk in ipairs(virtText) do
-                    local chunkText = chunk[1]
-                    local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-                    if targetWidth > curWidth + chunkWidth then
-                        table.insert(newVirtText, chunk)
-                    else
-                        chunkText = truncate(chunkText, targetWidth - curWidth)
-                        local hlGroup = chunk[2]
-                        table.insert(newVirtText, {chunkText, hlGroup})
-                        chunkWidth = vim.fn.strdisplaywidth(chunkText)
-                        -- str width returned from truncate() may less than 2nd argument, need padding
-                        if curWidth + chunkWidth < targetWidth then
-                            suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
-                        end
-                        break
-                    end
-                    curWidth = curWidth + chunkWidth
-                end
-                table.insert(newVirtText, {suffix, 'MoreMsg'})
-                return newVirtText
-            end,
-            provider_selector = function(_, _, _)
-                return 'treesitter'
-            end
-        })
-        vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-        vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-    end,
-    dependencies = 'kevinhwang91/promise-async',
-    keys = { 'zM' }
-}
+-- AddPlugin {
+--     'kevinhwang91/nvim-ufo',
+--     -- cond = function()
+--     --     return vim.o.foldmethod ~= 'marker'
+--     -- end,
+--     config = function()
+--         vim.o.foldcolumn = '1' -- '0' is not bad
+--         vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+--         vim.o.foldlevelstart = 99
+--         vim.o.foldenable = true
+--         require('ufo').setup({
+--             fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
+--                 local newVirtText = {}
+--                 local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+--                 local sufWidth = vim.fn.strdisplaywidth(suffix)
+--                 local targetWidth = width - sufWidth
+--                 local curWidth = 0
+--                 for _, chunk in ipairs(virtText) do
+--                     local chunkText = chunk[1]
+--                     local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+--                     if targetWidth > curWidth + chunkWidth then
+--                         table.insert(newVirtText, chunk)
+--                     else
+--                         chunkText = truncate(chunkText, targetWidth - curWidth)
+--                         local hlGroup = chunk[2]
+--                         table.insert(newVirtText, {chunkText, hlGroup})
+--                         chunkWidth = vim.fn.strdisplaywidth(chunkText)
+--                         -- str width returned from truncate() may less than 2nd argument, need padding
+--                         if curWidth + chunkWidth < targetWidth then
+--                             suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+--                         end
+--                         break
+--                     end
+--                     curWidth = curWidth + chunkWidth
+--                 end
+--                 table.insert(newVirtText, {suffix, 'MoreMsg'})
+--                 return newVirtText
+--             end,
+--             provider_selector = function(_, _, _)
+--                 return 'treesitter'
+--             end
+--         })
+--         vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+--         vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+--     end,
+--     dependencies = 'kevinhwang91/promise-async',
+--     keys = { 'zM' }
+-- }
 
 -- <~>
 --━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━   Formatting   ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</>
@@ -3688,13 +3688,13 @@ AddPlugin { -- STATUSCOL_OUT %@v:lua.ScFa@%C%T%#SignColumn#%*%=34%#SignColumn# %
             segments = {
                 { text = { '%C' }, click = 'v:lua.ScFa' },
                 { sign = { name = { 'todo.*' }, fillcharhl = 'LineNr' }, condition = { function() return TODO_COMMENTS_LOADED ~= nil end }, auto = true },
-                { sign = { name = { 'Diagnostic', fillcharhl = 'LineNr' }, auto = true } },
+                { sign = { name = { 'Diagnostic', fillcharhl = 'LineNr' }, auto = true } }, -- BUG: not showing
                 {
                     text = { builtin.lnumfunc },
                     condition = { true },
                     -- click = 'v:lua.ScLa',
                 },
-                { sign = { name = { 'Git' }, colwidth = 1, fillcharhl = 'LineNr' }, hl = 'LineNr' },
+                { sign = { name = { 'Git' }, colwidth = 1, fillcharhl = 'LineNr' }},
             },
             clickmod = "c",         -- modifier used for certain actions in the builtin clickhandlers:
             -- "a" for Alt, "c" for Ctrl and "m" for Meta.
