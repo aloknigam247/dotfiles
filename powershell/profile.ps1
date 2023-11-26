fc8a56a01ad0e670d3f4206450bb591975d158a9
Alok Nigam <aloknigm247@gmail.com>
2023-11-26 17:08:35 +0530

    Updated at 11/26/2023 17:08:35

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -60,7 +60,7 @@ function e ($arg) {
     if ( $ext -in $code_ext) {
         code $arg
     } else {
-        ~\scoop\shims\neovide.exe --multigrid --geometry=130x40 --notabs -- $arg
+        ~\scoop\shims\neovide.exe --multigrid --geometry=130x42 --notabs -- $arg
     }
 }
 
