0f0ed2498885b3bd1a94c762cf776862bc31e9d6
Alok Nigam <aloknigm247@gmail.com>
2024-01-09 13:09:44 +0530

    Updated at 01/09/2024 13:09:44

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -275,7 +275,7 @@ Set-PSReadLineKeyHandler -Key Alt+t -ScriptBlock { Invoke-FzfTabCompletion }
 Set-PsFzfOption -TabExpansion
 
 $env:FZF_DEFAULT_OPTS='
-    --height=~70% --layout=reverse --border=rounded --border-label="(Border)" --border-label-pos=5 --info=inline --prompt=" " --pointer="→" --preview="bat.exe --style=numbers {}" --preview-window="right,70%,border-rounded" --preview-label="(Preview)"
+    --height=~70% --layout=reverse --border=rounded --border-label=" FZF " --border-label-pos=5 --info=inline --prompt=" " --pointer="→" --preview="bat.exe --style=numbers {}" --preview-window="right,70%,border-rounded" --preview-label="(Preview)"
     --color=fg:#ffffff,bg:-1,hl:#71b7c2
     --color=fg+:#f0a330,bg+:#873df5,hl+:#a7daeb
     --color=info:#afaf87,prompt:#d7005f,pointer:#ff59f1
