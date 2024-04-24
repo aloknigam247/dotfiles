3cdfe1ca7d71fe039e9d2bfaaeae8450a164016a
Alok Nigam <aloknigam247@gmail.com>
2024-04-24 10:16:11 +0530

    Updated at 04/24/2024 10:16:11

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -351,7 +351,7 @@ function promptGen {
         }
         @{
             'params' = @{
-                'text'   = '$(Get-Location).ToString().Replace($HOME, "~")'
+                'text'   = '$((Get-Location).ToString().Replace($HOME, "~"))'
                 'fg'     = '#A3BCF9'
                 'styles' = "italic"
             }
