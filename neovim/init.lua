e20e808199240bd8b42374ad75c706d837255b7d
Alok Nigam <aloknigam247@gmail.com>
2024-05-25 17:35:42 +0530

    Updated at 05/25/2024 17:35:42

--- neovim/init.lua
+++ neovim/init.lua
@@ -4673,7 +4673,7 @@ addPlugin {
 -- BUG: debug working
 addPlugin {
 	'TheLeoP/powershell.nvim',
-	event = 'LspAttach',
+	lazy = false,
 	opts = {
 		bundle_path = vim.fn.stdpath "data" .. "\\mason\\packages\\powershell-editor-services"
 	}
