4ad6eafc008954b68c8324505a3dc2e4212dbc8d
Alok Nigam <aloknigam247@gmail.com>
2025-02-21 09:20:57 +0530

    Updated at 02/21/2025 09:20:57

--- neovim/init.lua
+++ neovim/init.lua
@@ -1743,54 +1743,54 @@ local function lightT(opts)
 	light(opts)
 end
 
--- addPlugin { "Shatur/neovim-ayu",                   event = "User ayu"                                          }
-addPlugin { "uloco/bluloco.nvim",                  event = "User bluloco",   dependencies = "rktjmp/lush.nvim" }
--- addPlugin { "catppuccin/nvim",                     event = "User catppuccin"                                   }
-addPlugin { "scottmckendry/cyberdream.nvim",       event = "User cyberdream"                                   }
-addPlugin { "projekt0n/github-nvim-theme",         event = "User github-theme"                                 }
-addPlugin { "HoNamDuong/hybrid.nvim",              event = "User hybrid"                                       }
-addPlugin { "nickkadutskyi/jb.nvim",               event = "User jb"                                           }
-addPlugin { "kaiuri/nvim-juliana",                 event = "User juliana"                                      }
-addPlugin { "rebelot/kanagawa.nvim",               event = "User kanagawa"                                     }
-addPlugin { "sho-87/kanagawa-paper.nvim",          event = "User kanagawa-paper"                               }
-addPlugin { "marko-cerovac/material.nvim",         event = "User material"                                     }
-addPlugin { "xero/miasma.nvim",                    event = "User miasma"                                       }
-addPlugin { "EdenEast/nightfox.nvim",              event = "User nightfox"                                     }
-addPlugin { "dgox16/oldworld.nvim",                event = "User oldworld"                                     }
-addPlugin { "NLKNguyen/papercolor-theme",          event = "User PaperColor"                                   }
--- addPlugin { "sainnhe/sonokai",                     event = "User sonokai"                                      }
--- addPlugin { "folke/tokyonight.nvim",               event = "User tokyonight"                                   }
--- addPlugin { "nxvu699134/vn-night.nvim",            event = "User vnight"                                     }
-addPlugin { "Mofiqul/vscode.nvim",                 event = "User vscode"                                       }
-addPlugin { "titanzero/zephyrium",                 event = "User zephyrium"                                    }
+addPlugin { "Shatur/neovim-ayu",             event = "User ayu"                                          }
+addPlugin { "uloco/bluloco.nvim",            event = "User bluloco",   dependencies = "rktjmp/lush.nvim" }
+addPlugin { "catppuccin/nvim",               event = "User catppuccin"                                   }
+addPlugin { "scottmckendry/cyberdream.nvim", event = "User cyberdream"                                   }
+addPlugin { "projekt0n/github-nvim-theme",   event = "User github-theme"                                 }
+addPlugin { "HoNamDuong/hybrid.nvim",        event = "User hybrid"                                       }
+addPlugin { "nickkadutskyi/jb.nvim",         event = "User jb"                                           }
+addPlugin { "kaiuri/nvim-juliana",           event = "User juliana"                                      }
+addPlugin { "rebelot/kanagawa.nvim",         event = "User kanagawa"                                     }
+addPlugin { "sho-87/kanagawa-paper.nvim",    event = "User kanagawa-paper"                               }
+addPlugin { "marko-cerovac/material.nvim",   event = "User material"                                     }
+addPlugin { "xero/miasma.nvim",              event = "User miasma"                                       }
+addPlugin { "EdenEast/nightfox.nvim",        event = "User nightfox"                                     }
+addPlugin { "dgox16/oldworld.nvim",          event = "User oldworld"                                     }
+addPlugin { "NLKNguyen/papercolor-theme",    event = "User PaperColor"                                   }
+addPlugin { "sainnhe/sonokai",               event = "User sonokai"                                      }
+addPlugin { "folke/tokyonight.nvim",         event = "User tokyonight"                                   }
+addPlugin { "nxvu699134/vn-night.nvim",      event = "User vnight"                                       }
+addPlugin { "Mofiqul/vscode.nvim",           event = "User vscode"                                       }
+addPlugin { "titanzero/zephyrium",           event = "User zephyrium"                                    }
 
--- -- darkT { "cyberdream",                 "_",                                                                                         }
--- -- lightT{ "cyberdream",                 "_",                                                                                         }
--- dark  { "ayu-dark",                   "ayu",          post = fixAyu                                                                }
--- darkT { "ayu-dark",                   "ayu",          post = fixAyu                                                                }
--- light { "ayu-light",                   "ayu",          post = fixAyu                                                                }
--- dark  { "bluloco",                    "_"                                                                                          }
--- light { "bluloco",                    "_"                                                                                          }
--- light { "bluloco",                    "_",              cfg = { transparent = true }                                               }
--- darkT { "bluloco",                    "_",              cfg = { transparent = true }                                               }
--- light { "catppuccin-latte",           "catppuccin"                                                                                 }
--- dark  { "catppuccin-macchiato",       "catppuccin"                                                                                 }
--- dark  { "duskfox",                    "nightfox"                                                                                   }
--- darkT { "duskfox",                    "nightfox",       cfg = { transparent = true }                                               }
-darkT { "github_dark",                "github-theme",   cfg = { options = { transparent = true } }                                 }
-dark  { "hybrid",                     "_"                                                                                          }
-dark  { "jb",                         "_"                                                                                          }
-dark  { "juliana",                    "_",            post = function() fixLineNr("#999999") end                                   }
-dark  { "kanagawa-wave",              "kanagawa"                                                                                   }
-darkT { "kanagawa-wave",              "kanagawa",       cfg = { transparent = true }                                               }
-light { "material",                   "_",            pre = function() preMaterial("lighter", "#CCEAE7") end, post = fixMaterial   }
-light { "PaperColor",                 "_"                                                                                          }
--- dark  { "sonokai",                    "_",            pre = function() vim.g.sonokai_style = "shusia" end                          }
--- dark  { "tokyonight-storm",           "tokyonight"                                                                                 }
--- darkT { "tokyonight-storm",           "tokyonight",     cfg = { transparent = true }                                               }
--- dark  { "vn-night",                   "_",            post = fixVnNight                                                            }
--- dark  { "vscode",                     "_",              post = fixVscode                                             }
-dark  { "zephyrium",                  "_"                                                                                          }
+-- dark  { "ayu-dark",             "ayu",          post = fixAyu                                                              }
+-- dark  { "bluloco",              "_"                                                                                        }
+-- dark  { "catppuccin-macchiato", "catppuccin"                                                                               }
+-- dark  { "duskfox",              "nightfox"                                                                                 }
+-- dark  { "hybrid",               "_"                                                                                        }
+-- dark  { "jb",                   "_"                                                                                        }
+-- dark  { "juliana",              "_",            post = function() fixLineNr("#999999") end                                 }
+-- dark  { "kanagawa-wave",        "kanagawa"                                                                                 }
+-- dark  { "sonokai",              "_",            pre = function() vim.g.sonokai_style = "shusia" end                        }
+-- dark  { "tokyonight-storm",     "tokyonight"                                                                               }
+-- dark  { "vn-night",             "_",            post = fixVnNight                                                          }
+-- dark  { "vscode",               "_",            post = fixVscode                                                           }
+-- dark  { "zephyrium",            "_"                                                                                        }
+-- darkT { "ayu-dark",             "ayu",          post = fixAyu                                                              }
+-- darkT { "bluloco",              "_",            cfg = { transparent = true }                                               }
+-- darkT { "cyberdream",           "_",                                                                                       }
+-- darkT { "duskfox",              "nightfox",     cfg = { transparent = true }                                               }
+-- darkT { "github_dark",          "github-theme", cfg = { options = { transparent = true } }                                 }
+-- darkT { "kanagawa-wave",        "kanagawa",     cfg = { transparent = true }                                               }
+-- darkT { "tokyonight-storm",     "tokyonight",   cfg = { transparent = true }                                               }
+light { "PaperColor",           "_"                                                                                        }
+light { "ayu-light",             "ayu",         post = fixAyu                                                              }
+light { "bluloco",              "_"                                                                                        }
+lightT{ "bluloco",              "_",            cfg = { transparent = true }                                               }
+-- light { "catppuccin-latte",     "catppuccin"                                                                               }
+light { "material",             "_",            pre = function() preMaterial("lighter", "#CCEAE7") end, post = fixMaterial }
+lightT{ "cyberdream",           "_",            cfg = { variant = "light", transparent = true }                            }
 
 ---Random colorscheme
 ---@param scheme_index? integer Index of colorscheme
@@ -4194,10 +4194,10 @@ addPlugin {
 					padding = { left = 0, right = 0 },
 					shorting_target = 40,
 					symbols = {
-						modified = icons.file_modified .. " ",
+						modified = icons.file_modified,
 						readonly = icons.file_readonly,
 						unnamed  = " " .. icons.file_unnamed .. " ",
-						newfile  = icons.file_newfile .. " "
+						newfile  = " " .. icons.file_newfile .. " "
 					}
 				}
 			},
