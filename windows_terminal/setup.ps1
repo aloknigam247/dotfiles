1f13544f49cd0734891ed91f4e077c9845bda1ac
Alok Nigam <aloknigam@microsoft.com>
2025-04-28 17:53:48 +0530

    Updated at 04/28/2025 17:53:48

--- windows_terminal/setup.ps1
+++ windows_terminal/setup.ps1
@@ -1,7 +1,15 @@
-$winget_pkgs = @(
-    "Microsoft.WindowsTerminal.Preview"
-)
+$term_installed = Get-AppxPackage -Name "Microsoft.WindowsTerminal"
 
-$files = @{
-    "settings.json" = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json";
+if ($null -eq $term_installed) {
+    $winget_pkgs = @(
+        "Microsoft.WindowsTerminal.Preview"
+    )
+
+    $files = @{
+        "settings.json" = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json";
+    }
+} else {
+    $files = @{
+        "settings.json" = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json";
+    }
 }
