5ff70fe03de79a156e2cbc6e0e48d313a579d80e
Alok Nigam <aloknigam247@gmail.com>
2025-05-05 13:46:43 +0530

    Updated at 05/05/2025 13:46:42

--- fonts/setup.ps1
+++ fonts/setup.ps1
@@ -1,13 +1,6 @@
-# $files = @{
-#     "BreeSerif-Regular.ttf"            = "$env:windir\Fonts\BreeSerif-Regular.ttf";
-#     "JetBrainsMonoNerdFont-Bold.ttf"   = "$env:windir\Fonts\JetBrainsMonoNerdFont-Bold.ttf";
-#     "JetBrainsMonoNerdFont-Italic.ttf" = "$env:windir\Fonts\JetBrainsMonoNerdFont-Italic.ttf";
-#     "JetBrainsMonoNerdFont-Medium.ttf" = "$env:windir\Fonts\JetBrainsMonoNerdFont-Medium.ttf";
-# }
+$fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
+$installed_fonts = $fonts.Items() | Select-Object -Property Name | Out-String
 
-$files = @{
-    "fonts\BreeSerif-Regular.ttf"            = "$env:LocalAppData\Microsoft\Windows\Fonts\BreeSerif-Regular.ttf";
-    "fonts\JetBrainsMonoNerdFont-Bold.ttf"   = "$env:LocalAppData\Microsoft\Windows\Fonts\JetBrainsMonoNerdFont-Bold.ttf";
-    "fonts\JetBrainsMonoNerdFont-Italic.ttf" = "$env:LocalAppData\Microsoft\Windows\Fonts\JetBrainsMonoNerdFont-Italic.ttf";
-    "fonts\JetBrainsMonoNerdFont-Medium.ttf" = "$env:LocalAppData\Microsoft\Windows\Fonts\JetBrainsMonoNerdFont-Medium.ttf";
+if ($installed_fonts.Contains("JetBrainsMono Nerd Font") -eq $false) {
+    Get-ChildItem .\fonts\ | ForEach-Object { $fonts.CopyHere($_.FullName) }
 }
