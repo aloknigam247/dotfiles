643a0150871595b73ac30dd8a618ecfab635b239
Alok Nigam <aloknigam247@gmail.com>
2025-06-29 20:47:40 +0530

    Updated at 06/29/2025 20:47:39

--- neovim/init.lua
+++ neovim/init.lua
@@ -2947,13 +2947,13 @@ vim.api.nvim_create_autocmd(
 -- <~>
 --━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━❰    Folding     ❱━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</>
 -- FEAT: create own folding code
--- Load statuscol plugin if not loaded
 -- Fold based on treesitter, then indent, then marker may be
 -- Create mapping to fold till level 1
 -- Use nvim-ufo python description
 
 -- Mapping to fold recursively for current buffer only
 vim.keymap.set('n', 'zz', function()
+	require("statuscol")
 	vim.wo.foldcolumn = "1"
 	vim.cmd('normal! zM')
 end, { noremap = true, silent = true, buffer = true })
@@ -3297,15 +3297,15 @@ addPlugin {
 			-- Navigation
 			map("n", "]c", function()
 				if vim.wo.diff then return "]c" end
-				vim.schedule(function() gs.next_hunk({preview = true}) end)
+				vim.schedule(function() gs.nav_hunk("next", { preview = true }) end)
 				return "<Ignore>"
-			end, {expr=true})
+			end, { expr=true })
 
 			map("n", "[c", function()
 				if vim.wo.diff then return "[c" end
-				vim.schedule(function() gs.prev_hunk({preview = true}) end)
+				vim.schedule(function() gs.nav_hunk("prev", { preview = true }) end)
 				return "<Ignore>"
-			end, {expr=true})
+			end, { expr=true })
 		end,
 		preview_config = {
 			border = dotted_border
