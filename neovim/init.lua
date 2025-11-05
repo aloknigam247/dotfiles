26236cab996a29f64e3cc7adddd1bab4c472dd75
Alok Nigam <aloknigam247@gmail.com>
2025-11-05 12:04:15 +0530

    Updated at 11/05/2025 12:04:14

--- neovim/init.lua
+++ neovim/init.lua
@@ -2974,46 +2974,115 @@ addPlugin {
 }
 
 addPlugin {
-	-- FEAT: https://github.com/oribarilan/lensline.nvim
-	-- FEAT: "Wansmer/symbol-usage.nvim",
-	"VidocqH/lsp-lens.nvim",
+	"Wansmer/symbol-usage.nvim",
 	event = "LspAttach",
-	opts = {
-		enable = true,
-		include_declaration = false, -- Reference include declaration
-		hide_zero_counts = true, -- Hide lsp sections which have no content
-		sections = {
-			definition = function(count)
-				if count == 1 then
-					return ""
+	config = function()
+		local function h(name) return vim.api.nvim_get_hl(0, { name = name }) end
+		vim.api.nvim_set_hl(0, "SymbolUsageRounding", { fg = h("CursorLine").bg, italic = true })
+		vim.api.nvim_set_hl(0, "SymbolUsageContent", { bg = h("CursorLine").bg, fg = h("Comment").fg, italic = true })
+		vim.api.nvim_set_hl(0, "SymbolUsageRef", { fg = h("Function").fg, bg = h("CursorLine").bg, italic = true })
+		vim.api.nvim_set_hl(0, "SymbolUsageDef", { fg = h("Type").fg, bg = h("CursorLine").bg, italic = true })
+		vim.api.nvim_set_hl(0, "SymbolUsageImpl", { fg = h("@keyword").fg, bg = h("CursorLine").bg, italic = true })
+
+
+		require("symbol-usage").setup({
+			text_format = function(symbol)
+				local res = {}
+
+				local round_start = { "", "SymbolUsageRounding" }
+				local round_end = { "", "SymbolUsageRounding" }
+
+				-- Indicator that shows if there are any other symbols in the same line
+				local stacked_functions_content = symbol.stacked_count > 0
+				and ("+%s"):format(symbol.stacked_count)
+				or ""
+
+				if symbol.references then
+					local usage = symbol.references <= 1 and "usage" or "usages"
+					local num = symbol.references == 0 and "no" or symbol.references
+					table.insert(res, round_start)
+					table.insert(res, { " ", "SymbolUsageRef" })
+					table.insert(res, { ("%s %s"):format(num, usage), "SymbolUsageContent" })
+					table.insert(res, round_end)
 				end
-				return "Definitions: " .. count
-			end,
-			references = function(count)
-				return "References: " .. count
-			end,
-			implements = function(count)
-				if count == 1 then
-					return ""
+
+				if symbol.definition then
+					if #res > 0 then
+						table.insert(res, { " ", "NonText" })
+					end
+					table.insert(res, round_start)
+					table.insert(res, { " ", "SymbolUsageDef" })
+					table.insert(res, { symbol.definition .. " defs", "SymbolUsageContent" })
+					table.insert(res, round_end)
 				end
-				return "Implements: " .. count
-			end,
-			git_authors = false --[[ function(latest_author, count)
-				return "󰀄 " .. latest_author .. (count - 1 == 0 and "" or (" + " .. count - 1))
-			end ]]
-		},
-		separator = " 󰧞 ",
-		target_symbol_kinds = {
-			vim.lsp.protocol.SymbolKind.Function,
-			vim.lsp.protocol.SymbolKind.Method,
-		},
-		decorator = function(line)
-			line = "󰧶 " .. line
-			return line
-		end,
-		ignore_filetype = {}
-	}
+
+				if symbol.implementation then
+					if #res > 0 then
+						table.insert(res, { " ", "NonText" })
+					end
+					table.insert(res, round_start)
+					table.insert(res, { " ", "SymbolUsageImpl" })
+					table.insert(res, { symbol.implementation .. " impls", "SymbolUsageContent" })
+					table.insert(res, round_end)
+				end
+
+				if stacked_functions_content ~= "" then
+					if #res > 0 then
+						table.insert(res, { " ", "NonText" })
+					end
+					table.insert(res, round_start)
+					table.insert(res, { " ", "SymbolUsageImpl" })
+					table.insert(res, { stacked_functions_content, "SymbolUsageContent" })
+					table.insert(res, round_end)
+				end
+
+				return res
+			end
+		})
+	end
 }
+
+-- addPlugin {
+-- 	-- FEAT: https://github.com/oribarilan/lensline.nvim
+-- 	-- FEAT: "Wansmer/symbol-usage.nvim",
+-- 	"VidocqH/lsp-lens.nvim",
+-- 	event = "LspAttach",
+-- 	opts = {
+-- 		enable = true,
+-- 		include_declaration = false, -- Reference include declaration
+-- 		hide_zero_counts = true, -- Hide lsp sections which have no content
+-- 		sections = {
+-- 			definition = function(count)
+-- 				if count == 1 then
+-- 					return ""
+-- 				end
+-- 				return "Definitions: " .. count
+-- 			end,
+-- 			references = function(count)
+-- 				return "References: " .. count
+-- 			end,
+-- 			implements = function(count)
+-- 				if count == 1 then
+-- 					return ""
+-- 				end
+-- 				return "Implements: " .. count
+-- 			end,
+-- 			git_authors = false --[[ function(latest_author, count)
+-- 				return "󰀄 " .. latest_author .. (count - 1 == 0 and "" or (" + " .. count - 1))
+-- 			end ]]
+-- 		},
+-- 		separator = " 󰧞 ",
+-- 		target_symbol_kinds = {
+-- 			vim.lsp.protocol.SymbolKind.Function,
+-- 			vim.lsp.protocol.SymbolKind.Method,
+-- 		},
+-- 		decorator = function(line)
+-- 			line = "󰧶 " .. line
+-- 			return line
+-- 		end,
+-- 		ignore_filetype = {}
+-- 	}
+-- }
 
 -- FEAT: https://github.com/hinell/lsp-timeout.nvim
 -- addPlugin {
