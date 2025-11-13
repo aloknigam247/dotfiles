19aefb8c7ea9e8c4d2e71725d5963d58984293ec
Alok Nigam <aloknigam247@gmail.com>
2025-11-13 11:07:06 +0530

    Updated at 11/13/2025 11:07:06

--- neovim/init.lua
+++ neovim/init.lua
@@ -1186,7 +1186,7 @@ addPlugin {
 						"}",
 						when = function(ctx)
 							local after = ctx:text_after_cursor(1)
-							return after == "" or after == "}" or after:match("[%s}]")
+							return after == "" or after:match("[%s{}()[]]")
 						end
 					}
 				},
@@ -1195,7 +1195,7 @@ addPlugin {
 						")",
 						when = function(ctx)
 							local after = ctx:text_after_cursor(1)
-							return after == "" or after == ")" or after:match("[%s)]")
+							return after == "" or after:match("[%s{}()[]]")
 						end
 					}
 				},
@@ -1204,9 +1204,9 @@ addPlugin {
 						"]",
 						space = function(ctx)
 							if ctx == nil then
-								vim.notify("Remove this check from blink.pairs", vim.log.levels.ERROR)
 								return true
 							end
+							vim.notify("Remove this check from blink.pairs", vim.log.levels.ERROR)
 							return ctx.ft ~= "markdown" or not ctx:is_before_cursor("- [")
 						end
 					}
@@ -2949,116 +2949,45 @@ addPlugin {
 	},
 }
 
--- FEAT: https://github.com/oribarilan/lensline.nvim
 addPlugin {
-	-- FEAT: do not show for no references
-	"Wansmer/symbol-usage.nvim",
+	"VidocqH/lsp-lens.nvim",
 	event = "LspAttach",
-	config = function()
-		local function h(name) return vim.api.nvim_get_hl(0, { name = name }) end
-		vim.api.nvim_set_hl(0, "SymbolUsageRounding", { fg = h("CursorLine").bg, italic = true })
-		vim.api.nvim_set_hl(0, "SymbolUsageContent", { bg = h("CursorLine").bg, fg = h("Comment").fg, italic = true })
-		vim.api.nvim_set_hl(0, "SymbolUsageRef", { fg = h("Function").fg, bg = h("CursorLine").bg, italic = true })
-		vim.api.nvim_set_hl(0, "SymbolUsageDef", { fg = h("Type").fg, bg = h("CursorLine").bg, italic = true })
-		vim.api.nvim_set_hl(0, "SymbolUsageImpl", { fg = h("@keyword").fg, bg = h("CursorLine").bg, italic = true })
-
-
-		require("symbol-usage").setup({
-			text_format = function(symbol)
-				local res = {}
-
-				local round_start = { "", "SymbolUsageRounding" }
-				local round_end = { "", "SymbolUsageRounding" }
-
-				-- Indicator that shows if there are any other symbols in the same line
-				local stacked_functions_content = symbol.stacked_count > 0
-				and ("+%s"):format(symbol.stacked_count)
-				or ""
-
-				if symbol.references then
-					local usage = symbol.references <= 1 and "usage" or "usages"
-					local num = symbol.references == 0 and "no" or symbol.references
-					table.insert(res, round_start)
-					table.insert(res, { " ", "SymbolUsageRef" })
-					table.insert(res, { ("%s %s"):format(num, usage), "SymbolUsageContent" })
-					table.insert(res, round_end)
+	opts = {
+		enable = true,
+		include_declaration = false, -- Reference include declaration
+		hide_zero_counts = true, -- Hide lsp sections which have no content
+		sections = {
+			definition = function(count)
+				if count == 1 then
+					return ""
 				end
-
-				if symbol.definition then
-					if #res > 0 then
-						table.insert(res, { " ", "NonText" })
-					end
-					table.insert(res, round_start)
-					table.insert(res, { " ", "SymbolUsageDef" })
-					table.insert(res, { symbol.definition .. " defs", "SymbolUsageContent" })
-					table.insert(res, round_end)
+				return "Definitions: " .. count
+			end,
+			references = function(count)
+				return "References: " .. count
+			end,
+			implements = function(count)
+				if count == 1 then
+					return ""
 				end
-
-				if symbol.implementation then
-					if #res > 0 then
-						table.insert(res, { " ", "NonText" })
-					end
-					table.insert(res, round_start)
-					table.insert(res, { " ", "SymbolUsageImpl" })
-					table.insert(res, { symbol.implementation .. " impls", "SymbolUsageContent" })
-					table.insert(res, round_end)
-				end
-
-				if stacked_functions_content ~= "" then
-					if #res > 0 then
-						table.insert(res, { " ", "NonText" })
-					end
-					table.insert(res, round_start)
-					table.insert(res, { " ", "SymbolUsageImpl" })
-					table.insert(res, { stacked_functions_content, "SymbolUsageContent" })
-					table.insert(res, round_end)
-				end
-
-				return res
-			end
-		})
-	end
+				return "Implements: " .. count
+			end,
+			git_authors = false --[[ function(latest_author, count)
+				return "󰀄 " .. latest_author .. (count - 1 == 0 and "" or (" + " .. count - 1))
+			end ]]
+		},
+		separator = " 󰧞 ",
+		target_symbol_kinds = {
+			vim.lsp.protocol.SymbolKind.Function,
+			vim.lsp.protocol.SymbolKind.Method,
+		},
+		decorator = function(line)
+			line = "󰧶 " .. line
+			return line
+		end,
+		ignore_filetype = {}
+	}
 }
-
--- addPlugin {
--- 	"VidocqH/lsp-lens.nvim",
--- 	event = "LspAttach",
--- 	opts = {
--- 		enable = true,
--- 		include_declaration = false, -- Reference include declaration
--- 		hide_zero_counts = true, -- Hide lsp sections which have no content
--- 		sections = {
--- 			definition = function(count)
--- 				if count == 1 then
--- 					return ""
--- 				end
--- 				return "Definitions: " .. count
--- 			end,
--- 			references = function(count)
--- 				return "References: " .. count
--- 			end,
--- 			implements = function(count)
--- 				if count == 1 then
--- 					return ""
--- 				end
--- 				return "Implements: " .. count
--- 			end,
--- 			git_authors = false --[[ function(latest_author, count)
--- 				return "󰀄 " .. latest_author .. (count - 1 == 0 and "" or (" + " .. count - 1))
--- 			end ]]
--- 		},
--- 		separator = " 󰧞 ",
--- 		target_symbol_kinds = {
--- 			vim.lsp.protocol.SymbolKind.Function,
--- 			vim.lsp.protocol.SymbolKind.Method,
--- 		},
--- 		decorator = function(line)
--- 			line = "󰧶 " .. line
--- 			return line
--- 		end,
--- 		ignore_filetype = {}
--- 	}
--- }
 
 addPlugin {
 	"Zeioth/garbage-day.nvim",
@@ -3281,14 +3210,40 @@ addPlugin {
 	end
 }
 
--- FEAT: create custom code actions
 addPlugin {
 	"nvimtools/none-ls.nvim",
 	dependencies = "nvim-lua/plenary.nvim",
 	config = function()
+		-- https://github.com/Zeioth/none-ls-external-sources.nvim
 		local null_ls = require("null-ls")
 		null_ls.setup({
-			-- https://github.com/Zeioth/none-ls-external-sources.nvim
+			sources = {
+				null_ls.builtins.completion.tags
+			}
+		})
+
+		-- Code action to generate docstrings for python
+		null_ls.register({
+			method = null_ls.methods.CODE_ACTION,
+			name = "GenerateDocstrings",
+			filetypes = { "python" },
+			generator = {
+				fn = function()
+					local diags = vim.diagnostic.get(0, { lnum = vim.api.nvim_win_get_cursor(0)[1] - 1 })
+
+					if vim.iter(pairs(diags)):any(function(_,v) return v.code == "D103" end) then
+						return {
+							{
+								title = "Generate docstring",
+								action = function()
+									vim.cmd("Neogen")
+								end
+							}
+						}
+					end
+					return {}
+				end
+			}
 		})
 	end
 }
