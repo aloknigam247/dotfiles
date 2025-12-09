b9c5be1fb7b73abc19e3ff76453f4018516ec80a
Alok Nigam <aloknigam247@gmail.com>
2025-12-09 18:26:04 +0530

    Updated at 12/09/2025 18:26:04

--- a.lua
+++
@@ -1,3 +0,0 @@
-local msg = { { "MiniHipatterns-FIX", "Title" }, { " " }, { "(19)", "Comment" }, { " " }, { "highlight", "Type" }, { " " }, { "DiagnosticError", "DiagnosticError" }, { "\n" }, { "indent_blankline", "Title" }, { " " }, { "(14)", "Comment" }, { " " }, { "virt_text", "Type" }, { " " }, { " " }, { "\n" }, { "indent_blankline", "Title" }, { " " }, { "(14)", "Comment" }, { " " }, { "virt_text", "Type" }, { " " }, { " " }, { "\n" } }
-
-vim.api.nvim_echo(msg, true, {})
