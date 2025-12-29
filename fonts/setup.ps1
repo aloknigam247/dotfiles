1cc648d2a9d4bbf1c918702151973a081be2b33d
Alok Nigam <aloknigam247@gmail.com>
2025-12-29 16:43:46 +0530

    Updated at 12/29/2025 16:43:46

--- fonts/setup.ps1
+++ fonts/setup.ps1
@@ -1,6 +1,5 @@
 $fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
 $installed_fonts = $fonts.Items() | Select-Object -Property Name | Out-String
-# FEAT: better balance of thin and bold typefaces
 if ($installed_fonts.Contains("JetBrainsMono Nerd Font") -eq $false) {
     Get-ChildItem .\fonts\ | ForEach-Object { $fonts.CopyHere($_.FullName) }
 }
