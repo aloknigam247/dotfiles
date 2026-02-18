7db4a56999f6b4b16ccdb5400bfd9161c0851eaa
Alok Nigam <aloknigam247@gmail.com>
2026-02-18 11:31:55 +0530

    Updated at 02/18/2026 11:31:54

--- neovim/init.lua
+++ neovim/init.lua
@@ -2187,7 +2187,7 @@ addPlugin {
 							return
 								not utils.is_command_line()
 								or utils.is_command_line({ "/", "?" })
-								or utils.in_ex_context(require("blink.cmp.sources.cmdline.constants").ex_search_commands)
+								-- or utils.in_ex_context(require("blink.cmp.sources.cmdline.constants").ex_search_commands) -- FIX: not working
 						end
 					}
 				},
