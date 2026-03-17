a36ef49aeaebafb342a8274d27a4cef6af363aef
Alok Nigam <aloknigam247@gmail.com>
2026-03-17 08:19:24 +0530

    Updated at 03/17/2026 08:19:23

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -280,7 +280,8 @@ New-Alias -Name pacman -Value D:\Scoop\apps\msys2\current\usr\bin\pacman.exe -Er
 # ─[ Common ]──────────────────────────────────────────────────────────
 New-Alias -Name "/" -Value rg -ErrorAction SilentlyContinue
 New-Alias -Name "//" -Value fd -ErrorAction SilentlyContinue
-function bg { wt -w 0 new-tab -d $PWD.Path --colorScheme $current_theme cmd /C "$args" }
+function bg { wt -f -d $PWD.Path --colorScheme $current_theme cmd /C "$args" }
+New-Alias -Name "&" -Value bg -ErrorAction SilentlyContinue
 
 # ╭───────────────────╮
 # │ Generic Functions │
